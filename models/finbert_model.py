91163d2e1b7e39759cb50416f104b7b59ab8ec13
{gitUsername} <{gitEmail}>
2025-07-27 03:39:19 +0000

    added score_df function

--- models/finbert_model.py
+++ models/finbert_model.py
@@ -1,1 +1,18 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import numpy as np
 
+tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
+model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
+
+def score(text):
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+    outputs = model(**inputs)
+    scores = torch.nn.functional.softmax(outputs.logits, dim=1)
+    labels = ['negative', 'neutral', 'positive']
+    return dict(zip(labels, scores[0].detach().numpy()))
+
+def score_df(df):
+    df["sent_scores"] = df['description'].apply(score)
+    df['category'] = df['sent_scores'].apply(lambda x: max(x, key=x.get))
+    return df
